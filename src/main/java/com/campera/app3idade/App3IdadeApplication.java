818ec579eea039aa1fd40417daeaa1a085ca0d94
Bruno Campera <brunocmpr@gmail.com>
2022-07-23 00:34:29 -0300

    Initialize Maven Project

---
+++ src/main/java/com/campera/app3idade/App3IdadeApplication.java
@@ -0,0 +1,13 @@
+package com.campera.app3idade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App3IdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App3IdadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campera/app3idade/App3IdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campera.app3idade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App3IdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
